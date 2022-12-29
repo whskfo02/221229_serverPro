eb596024227b553c1ce052c7e8feb19f846c691a
whskfo02 <whskfo02@gmail.com>
2022-12-29 11:43:32 +0900

    first commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,6 +2,6 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("20221229_서버프로그램 구현 : 조나래");
+		System.out.println("221229_서버프로그램 구현 : 조나래");
 	}
 }
